844b70bf3495b8d87ac6255298f4ed9a7b8f048d
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-07-20 10:22:36 +0530

    Improve UI, add icons for representing the buttons

--- script.js
+++ script.js
@@ -43,25 +43,51 @@ function playRound(playerSelection, computerSelection) {
   const outcome = findOutcome(playerSelection, computerSelection);
 
   if (outcome === "lost") {
-    computer.score += 2;
-
-    return `You lost! ${capitalize(computerSelection)} beats 
-    ${capitalize(playerSelection)}`;
-  } else if (outcome === "won") {
-    player.score += 2;
-
-    return `You won! ${capitalize(playerSelection)} beats 
-    ${capitalize(computerSelection)}`;
-  } else {
-    player.score++;
     computer.score++;
-    return `You tie! ${capitalize(computerSelection)} ties with 
-    ${capitalize(playerSelection)}`;
+
+    return (
+      `You lost! ${capitalize(computerSelection)} beats ` +
+      capitalize(playerSelection)
+    );
+  } else if (outcome === "won") {
+    player.score++;
+
+    return (
+      `You won! ${capitalize(playerSelection)} beats ` +
+      capitalize(computerSelection)
+    );
+  } else {
+    return (
+      `You tie! ${capitalize(computerSelection)} ties with ` +
+      capitalize(playerSelection)
+    );
+  }
+}
+
+function declareWinner() {
+  const result = document.querySelector(".result");
+  if (player.score > computer.score) {
+    result.innerText = "You win!";
+    result.classList.add("win");
+  } else if (player.score < computer.score) {
+    result.innerText = "You lose!!!";
+    result.classList.add("lose");
+  } else {
+    result.innerText = "It's a draw!";
+  }
+  result.hidden = false;
+
+  // disabling the buttons
+  const buttons = document.querySelectorAll(".buttons-section button");
+
+  for (let button of buttons) {
+    button.disabled = true;
   }
 }
 
 function resetScores() {
   players.forEach((player) => (player.score = 0));
+  document.querySelector(".result").classList.remove("win", "lose");
 }
 
 function game(playerSelection) {
@@ -69,6 +95,7 @@ function game(playerSelection) {
 
   const currentStatus = document.querySelector(".current-status");
   currentStatus.innerText = currentGameResult;
+  currentStatus.hidden = false;
 
   const playerScore = document.querySelector(".player-score span");
   const computerScore = document.querySelector(".computer-score span");
@@ -80,15 +107,8 @@ function game(playerSelection) {
     const currentScores = document.querySelector(".current-scores");
     currentScores.hidden = false;
   }
-  if (player.score + computer.score >= 10) {
-    const result = document.querySelector(".result");
-    if (player.score > computer.score) {
-      result.innerText = "The winner is you!";
-    } else if (player.score < computer.score) {
-      result.innerText = "The winner is the computer!";
-    } else {
-      result.innerText = "It is a tie!";
-    }
+  if (player.score === 10 || computer.score === 10) {
+    declareWinner();
   }
 }
 
